bed9abc0712921181eeb0778b5cddc6989f3ec11
Antoine Brusco <a7brusco@enib.fr>
2022-01-19 15:59:52 +0100

    trigger

--- Job-Duration-Metrics.py
+++ Job-Duration-Metrics.py
@@ -40,7 +40,6 @@ class DurationMetrics:
         # TODO: get job duration
         jenkinsJobs = self.server.get_all_jobs()
         myJob = self.server.get_job_info('python-test', 0, True)
-        print(myJob)
         myJobBuilds = myJob.get('builds')
         for build in myJobBuilds:
             buildNumber = build.get('number')
